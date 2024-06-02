8341dd9c5ead9dcf8227205669980498527bf029
Raquel Dávila <raqueldaviwek28@hotmail.com>
2024-06-02 13:58:05 -0300

    #1-T rutas en app.py

--- app /app.py
+++ app /app.py
@@ -3,27 +3,27 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 @app.route('/')
-def index():
-    return render_template('index.html')
+def home():
+    return render_template('home.html')
 
 @app.route('/habitaciones')
 def tipos_habitaciones():
     return render_template('habitaciones.html')
 
-@app.route('/introduction')
-def intro():
-    return render_template('introduction.html')
+@app.route('/about') 
+def about():
+    return render_template('about.html')
 
 @app.route('/gallery')
 def gallery():
     return render_template('gallery.html')
 
-@app.route('/contact')
-def contact():
+@app.route('/cancelar-reserva')
+def cancerlar_reserva():
     return render_template('cancelacion.html')
 
 @app.route('/detalles')
-def details():
+def detalle_habitacion():
     return render_template('room-details.html')
 
 if __name__ == '__main__':
