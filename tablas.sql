9087b94a358f1486bbe6ae679900579f9f101bae
Em Ca <ecastares@fi.uba.ar>
2024-06-02 19:00:37 -0300

    #1-DB Se agregan registros a la tabla Habitaciones_Particulares, se configura el archivo para poder importarlo desde Xampp y que se cree la base de datos.

--- tablas.sql
+++ tablas.sql
@@ -1,3 +1,6 @@
+CREATE DATABASE dreaminn_db;
+use dreaminn_db;
+
 CREATE TABLE Reserva_Especifica (
     id INT PRIMARY KEY AUTO_INCREMENT,
     nombre VARCHAR(100) NOT NULL,
@@ -20,7 +23,22 @@ CREATE TABLE Tipo_Habitaciones (
     precio INT NOT NULL
 );
 
-INSERT INTO Tipo_Habitaciones VALUES ('Simple','Tiene una cama, una television,un escritorio y un frigobar',40000);
-INSERT INTO Tipo_Habitaciones VALUES ('Doble','Tiene dos camas, una television,un escritorio y un frigobar',70000);
-INSERT INTO Tipo_Habitaciones VALUES ('Familiar','Tiene cuatro camas, una television,dos escritorio y un frigobar',100000);
+INSERT INTO Tipo_Habitaciones VALUES ('Simple','Tiene una cama, una television, un escritorio y un frigobar',40000);
+INSERT INTO Tipo_Habitaciones VALUES ('Doble','Tiene dos camas, una television, un escritorio y un frigobar',70000);
+INSERT INTO Tipo_Habitaciones VALUES ('Familiar','Tiene cuatro camas, una television, dos escritorios y un frigobar',100000);
 
+INSERT INTO Habitaciones_Particulares VALUES (1, 'Simple', false);
+INSERT INTO Habitaciones_Particulares VALUES (2, 'Simple', false);
+INSERT INTO Habitaciones_Particulares VALUES (3, 'Simple', false);
+INSERT INTO Habitaciones_Particulares VALUES (4, 'Simple', false);
+INSERT INTO Habitaciones_Particulares VALUES (5, 'Simple', false);
+INSERT INTO Habitaciones_Particulares VALUES (6, 'Doble', false);
+INSERT INTO Habitaciones_Particulares VALUES (7, 'Doble', false);
+INSERT INTO Habitaciones_Particulares VALUES (8, 'Doble', false);
+INSERT INTO Habitaciones_Particulares VALUES (9, 'Doble', false);
+INSERT INTO Habitaciones_Particulares VALUES (10, 'Doble', false);
+INSERT INTO Habitaciones_Particulares VALUES (11, 'Familiar', false);
+INSERT INTO Habitaciones_Particulares VALUES (12, 'Familiar', false);
+INSERT INTO Habitaciones_Particulares VALUES (13, 'Familiar', false);
+INSERT INTO Habitaciones_Particulares VALUES (14, 'Familiar', false);
+INSERT INTO Habitaciones_Particulares VALUES (15, 'Familiar', false);
