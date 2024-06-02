02ec376bec197f972f8625feafec899ef09b553f
Lucas Raffinetti Tempone <lraffinetti@fi.uba.ar>
2024-06-02 17:03:36 -0300

    #1-DB Completando tabla Tipo_Habitaciones y modificando su nombre a este mismo.

--- tablas.sql
+++ tablas.sql
@@ -8,15 +8,19 @@ CREATE TABLE Reserva_Especifica (
     fecha_salida DATE NOT NULL
 );
 
-
 CREATE TABLE Habitaciones_Particulares (
     numero_habitacion INT PRIMARY KEY NOT NULL,
     tipo_habitacion VARCHAR(50) NOT NULL,
     estado BOOLEAN NOT NULL
 );
 
-CREATE TABLE Tipo_Habitacion (
+CREATE TABLE Tipo_Habitaciones (
     tipo_habitacion VARCHAR(50) PRIMARY KEY NOT NULL,
     descripcion VARCHAR(300) NOT NULL,
     precio INT NOT NULL
 );
+
+INSERT INTO Tipo_Habitaciones VALUES ('Simple','Tiene una cama, una television,un escritorio y un frigobar',40000);
+INSERT INTO Tipo_Habitaciones VALUES ('Doble','Tiene dos camas, una television,un escritorio y un frigobar',70000);
+INSERT INTO Tipo_Habitaciones VALUES ('Familiar','Tiene cuatro camas, una television,dos escritorio y un frigobar',100000);
+
