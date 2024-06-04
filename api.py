593b94e4006a44cfaa19d6adc399c12140f6cf87
EmCasta <ecastares@fi.uba.ar>
2024-06-04 15:48:15 -0300

    #1-A Servicio API para hacer una reserva Se agregó al archivo api.py el método POST para agregar una reserva a la tabla Reservas_Especificas

--- api.py
+++ api.py
@@ -7,13 +7,56 @@ app = Flask(__name__)
 engine = create_engine("mysql+mysqlconnector://root@localhost:3305/dreaminn_db")
 #nota: la base de datos MySQL debe correr en el puerto 3305 (configurar el puerto de XAMPP, en mysql config)
     
+
+
+@app.route("/generar_reserva", methods = ["POST"])
+def generar_reserva():
+    """Genera un nuevo registro en la tabla Reserva_Especifica con los datos en formato JSON pasados por el body de la
+    request (nombre, email, tipo_habitacion, numero_habitacion, fecha_entrada, fecha_salida). Cambia el estado de la habitación
+    a true en la tabla Habitaciones_Particulares, y devuelve un JSON con claves: mensaje, id_reserva.
+    Nota: para que la tabla tome correctamente la fecha, se debe ingresar en formato YYYY-MM-DD."""
+
+    reserva = request.get_json()
+    with engine.connect() as conexion:
+
+        validacion = text(f"SELECT * FROM Reserva_Especifica WHERE numero_habitacion = {reserva['numero_habitacion']};")
+
+        try:
+            val_resultado = conexion.execute(validacion)
+
+            if val_resultado.rowcount != 0:            # si ya hay una reserva a ese número de habitación, no se puede reservar
+                return jsonify({"mensaje": f"La habitacion {reserva['numero_habitacion']} ya esta ocupada"}), 400
+            
+            # agregar reserva
+            query = text(f"INSERT INTO Reserva_Especifica (nombre, email, tipo_habitacion, numero_habitacion, fecha_entrada, fecha_salida) VALUES ('{reserva['nombre']}', '{reserva['email']}', '{reserva['tipo_habitacion']}', {reserva['numero_habitacion']}, '{reserva['fecha_entrada']}', '{reserva['fecha_salida']}');")
+            conexion.execute(query)
+            conexion.commit()
+
+            # obtener id de reserva
+            query_id = text(f"SELECT id FROM Reserva_Especifica WHERE numero_habitacion = {reserva['numero_habitacion']};")
+            res_id = conexion.execute(query_id)
+            id = res_id.fetchone().id
+
+            # cambiar estado de habitación a true (ocupado)
+            query_estado_habitacion = text(f"UPDATE Habitaciones_Particulares SET estado = true WHERE numero_habitacion = {reserva['numero_habitacion']};")
+            conexion.execute(query_estado_habitacion)
+            conexion.commit()
+
+            # devolver mensaje de éxito e id
+            return jsonify({"mensaje": "La reserva se ha realizado con exito", "id_reserva": id}), 201
+
+        except SQLAlchemyError as error:
+            return jsonify({"mensaje": f"Se ha producido un error: {error}"}), 500
+
+
+
 @app.route("/Habitaciones_Particulares", methods=["GET"])
 def mostrar_habitaciones() -> dict:
     """
     Se conecta a la base de datos dreaminn, y hace una consulta en la cual devuelve las habitaciones que no estan ocupadas
     """
     conn = engine.connect()
-    query = "SELECT * FROM Habitaciones_Particulares WHERE estado = 1;"
+    query = "SELECT * FROM Habitaciones_Particulares WHERE estado = 0;"
     try:
         habs = conn.execute(text(query))
         conn.close()
@@ -30,6 +73,8 @@ def mostrar_habitaciones() -> dict:
         datos.append(hab)
     
     return jsonify(datos), 200
+
+
 
 @app.route("/Tipo_Habitaciones", methods = ['GET'])
 def mostrar_tipos():
@@ -52,6 +97,8 @@ def mostrar_tipos():
 
     return jsonify(datos), 200
 
+
+
 @app.route("/cancelar-reserva/<id>", methods=['DELETE','PATCH'])
 def cancelar_reserva(id):
     conn = engine.connect()
@@ -73,6 +120,8 @@ def cancelar_reserva(id):
             return jsonify({"message": "No existe la reserva de tal ID"}), 404
     except SQLAlchemyError as err:
         return jsonify(str(err.__cause__))
+
+
 
 if __name__ == "__main__":
     app.run("127.0.0.1", port = 5000, debug = True)
