6d8ef97ad07d3613ca4fb0cff8df0f4a1a0d61e1
EmCasta <ecastares@fi.uba.ar>
2024-06-07 12:42:43 -0300

    #3-T Actualización de base de datos en archivo tablas.sql Se quitaron los INSERT INTO (no son necesarios ya que la tabla se modifica desde la API) y se agregaron dos columnas a la tabla reservas: cantidad_personas y cantidad_noches.

--- tablas.sql
+++ tablas.sql
@@ -12,22 +12,11 @@ CREATE TABLE habitaciones (
 CREATE TABLE reservas (
     id INT PRIMARY KEY AUTO_INCREMENT,
     habitacion INT NOT NULL,
+    cantidad_personas INT NOT NULL,
+    cantidad_noches INT NOT NULL,
     nombre VARCHAR(100) NOT NULL,
-    appellido VARCHAR(100) NOT NULL,
+    apellido VARCHAR(100) NOT NULL,
     email VARCHAR(100) NOT NULL,
     fecha_ingreso DATE NOT NULL,
-    fecha_salida DATE NOT NULL,
     FOREIGN KEY (habitacion) REFERENCES habitaciones(habitacion)
 );
-
-INSERT INTO habitaciones VALUES (100,4,100000,"Una habitacion con cuatro camas individuales.","estandar");
-INSERT INTO habitaciones VALUES (101,6,150000,"Una habitacion con una cama doble y otra habitacion con cuatro camas individuales .","estandar");
-INSERT INTO habitaciones VALUES (102,2,70000,"Una habitacion con una cama doble.","estandar");
-INSERT INTO habitaciones VALUES (103,5,100000,"Una habitacion con cinco camas individuales.","estandar");
-INSERT INTO habitaciones VALUES (104,4,200000,"Una habitacion con una cama doble, un escritorio y otra habitacion con dos camas individuales.Ambas cuentan con television y vista al mar","deluxe");
-INSERT INTO habitaciones VALUES (105,2,150000,"Una habitacion con una cama doble, un escritorio,una television y vista al mar.","deluxe");
-INSERT INTO habitaciones VALUES (106,2,70000,"Una habitacion con una cama doble y dos camas individuales.","estandar");
-INSERT INTO habitaciones VALUES (107,4,100000,"Una habitacion con dos camas individuales.","estandar");
-
-
-
